ae49cef20ab87541f29dcc67c4bc4ab521d3d425
Hartmut Behrens <hartmut.behrens@gmail.com>
2020-04-27 12:04:18 +0200

    cmake: compile and copy shaders

--- GLSL/compile.sh
+++ GLSL/compile.sh
@@ -1,8 +1,9 @@
-#!/bin/bash
+#!/usr/bin/env bash
 if [[ -z "${VULKAN_SDK_PATH}" ]]; then
   echo "ERROR: Please set VULKAN_SDK_PATH (/path/to/vulkan-sdk/x.y.z.w/x86_64)"
   exit 1
 fi
-$VULKAN_SDK_PATH/bin/glslc -fshader-stage=vert vert.glsl -o vert.spv
-$VULKAN_SDK_PATH/bin/glslc -fshader-stage=frag frag.glsl -o frag.spv
+SCRIPT_DIR=`dirname $0`
+$VULKAN_SDK_PATH/bin/glslc -fshader-stage=vert $SCRIPT_DIR/vert.glsl -o $SCRIPT_DIR/vert.spv
+$VULKAN_SDK_PATH/bin/glslc -fshader-stage=frag $SCRIPT_DIR/frag.glsl -o $SCRIPT_DIR/frag.spv
 
