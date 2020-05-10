e288d1e0b822483613c8f069bcade952cc1763d1
Hartmut Behrens <hartmut.behrens@gmail.com>
2020-05-10 18:45:24 +0200

    createBuffer: added TODO to fix very bad memory allocation pattern

--- src/Vk/Pipeline.cpp
+++ src/Vk/Pipeline.cpp
@@ -389,6 +389,8 @@ namespace vkShmup {
         //VkDeviceMemory stagingBufferMemory;
         VmaAllocation stagingBufferMemory;
         VmaAllocationCreateInfo stagingAllocInfo = {};
+        // TODO: this is doing a very bad type of memory allocation (see https://developer.nvidia.com/vulkan-memory-management)
+        // TODO: fix
         stagingAllocInfo.requiredFlags = VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT;
         createBuffer(bufferSize, VK_BUFFER_USAGE_TRANSFER_SRC_BIT, stagingAllocInfo, stagingBuffer, stagingBufferMemory);
 
